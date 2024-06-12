02c21d2a06270aeba23237ce865d007c6be33e7c
Danil L <danillitvinenkod00@gmail.com>
2024-06-13 00:35:22 +0300

    Added theme change function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/header/header"
 import { SettingsBar } from "./components/settings-bar/settings-bar"
+import onChange from "on-change"
 
 class App {
   constructor() {
     this.app = document.getElementById('root')
+    this.appState = onChange(this.appState, this.appStateHook.bind(this))
     this.render()
   }
 
@@ -11,12 +13,19 @@ class App {
     theme: 'light'
   }
 
+  appStateHook(path) {
+    if (path === "theme") {
+      this.render()
+    }
+  }
+
   render() {
+    this.app.innerHTML = ""
     const main = document.createElement('div')
-    this.appState.theme === 'dark' ? this.app.classList.add('dark-theme') : ""
+    this.appState.theme === 'dark' ? this.app.classList.add('dark-theme') : this.app.classList.remove('dark-theme')
     main.classList.add('main')
     this.renderHeader()
-    main.append(new SettingsBar().render())
+    main.append(new SettingsBar(this.appState).render())
     this.app.append(main)
     this.openSettingsBar()
   }
--- src/components/settings-bar/settings-bar.js
+++ src/components/settings-bar/settings-bar.js
@@ -3,22 +3,33 @@ import { DivComponent } from "../../common/div-component";
 
 
 export class SettingsBar extends DivComponent {
-  constructor() {
+  constructor(appState) {
     super()
+    this.appState = appState
   }
 
   render() {
     this.el.classList.add('settings-bar')
 
+    const themes = {
+      "light": "Светлая",
+      "dark": "Темная"
+    }
+
     this.el.innerHTML = `
     <div class="settings-bar__theme">
-      <span>Тема: Светлая</span>
+      <span>Тема: ${themes[this.appState.theme]}</span>
       <label class="switch">
-        <input type="checkbox">
+        <input type="checkbox" class="switch-theme__input" ${this.appState.theme === "dark" ? "checked" : ""}>
         <span class="slider"></span>
       </label>
     </div>
     `
+
+    this.el.querySelector('.switch-theme__input').addEventListener('click', () => {
+      if (this.appState.theme === "light") this.appState.theme = 'dark'
+      else if (this.appState.theme === "dark") this.appState.theme = 'light'
+    })
 
     return this.el
   }
